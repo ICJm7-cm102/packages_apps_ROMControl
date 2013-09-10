6202d8fbd3d0f5ec08d043594f61162365d54fc5
LorD ClockaN <davor@losinj.com>
2013-09-10 13:08:42 +0200

    Revert "RC: Option to hide Camera widget from lockscreen (2/2)"
    This reverts commit 24c1a43b626defee409a7c75c14302b30a1b4906.

--- src/com/aokp/romcontrol/fragments/UserInterface.java
+++ src/com/aokp/romcontrol/fragments/UserInterface.java
@@ -84,7 +84,6 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
     private static final CharSequence PREF_SHOW_OVERFLOW = "show_overflow";
     private static final CharSequence PREF_VIBRATE_NOTIF_EXPAND = "vibrate_notif_expand";
     private static final CharSequence PREF_LONGPRESS_TO_KILL = "longpress_to_kill";
-    private static final CharSequence PREF_CAMERA_WIDGET_HIDE = "camera_widget_hide";
     private static final CharSequence PREF_RECENT_KILL_ALL = "recent_kill_all";
     private static final CharSequence PREF_RECENT_GOOGLE_ASSIST = "recent_google_assist";
     private static final CharSequence PREF_RAM_USAGE_BAR = "ram_usage_bar";
@@ -126,7 +125,6 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
     CheckBoxPreference mShowActionOverflow;
     CheckBoxPreference mVibrateOnExpand;
     CheckBoxPreference mLongPressToKill;
-    CheckBoxPreference mCameraWidget;
     CheckBoxPreference mRecentKillAll;
     CheckBoxPreference mRecentGoog;
     CheckBoxPreference mRamBar;
@@ -224,10 +222,6 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
         mRecentKillAll = (CheckBoxPreference) findPreference(PREF_RECENT_KILL_ALL);
         mRecentKillAll.setChecked(Settings.System.getBoolean(mContentResolver,
                 Settings.System.RECENT_KILL_ALL_BUTTON, false));
-
-        mCameraWidget = (CheckBoxPreference) findPreference(PREF_CAMERA_WIDGET_HIDE);
-        mCameraWidget.setChecked(Settings.System.getBoolean(mContentResolver,
-                Settings.System.CAMERA_WIDGET_HIDE, false));
 
         mRecentGoog = (CheckBoxPreference) findPreference(PREF_RECENT_GOOGLE_ASSIST);
         mRecentGoog.setChecked(Settings.System.getBoolean(mContentResolver,
@@ -521,11 +515,6 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
             boolean checked = ((TwoStatePreference) preference).isChecked();
             Settings.System.putBoolean(mContentResolver,
                     Settings.System.RECENT_KILL_ALL_BUTTON, checked);
-            return true;
-        } else if (preference == mCameraWidget) {
-            boolean checked = ((TwoStatePreference) preference).isChecked();
-            Settings.System.putBoolean(mContentResolver,
-                    Settings.System.CAMERA_WIDGET_HIDE, checked);
             return true;
         } else if (preference == mRecentGoog) {
             boolean checked = ((TwoStatePreference) preference).isChecked();
